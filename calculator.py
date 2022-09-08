1231a28026c41b9e9b4d1295a7516c07e8bae4e6
Kim <klucas96@naver.com>
2022-09-08 21:08:40 +0900

    Create index.js

--- calculator.py
+++ calculator.py
@@ -7,3 +7,5 @@ def subtract(a, b):
     print(a - b)
     return a - b
 
+def multiply(a, b):
+    return a * b
---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * 이름, 나이를 합쳐서 문자로 만들어주는 함수!
+ * @param {string} 이름을 적어주세요 ㅎㅎ
+ * @param {number} 몇 살이니?
+ * @returns 두 개 합쳐서 문자로 만들어준다.
+ */
+
+function hello(name, age) {
+  return name + age;
+}
+
+hello("kim", 20);
